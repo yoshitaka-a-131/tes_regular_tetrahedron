e54c2addcc0b53dc3fb1e62f205fa7ac32c93c27
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2025-04-18 00:39:44 +0900

    Add files via upload

---
+++ desktop/AizuSpider.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /opt/Algoryx/AGX-2.38.0.0/setup_env.bash
+export LD_LIBRARY_PATH=/opt/Algoryx/AGX-2.38.0.0/lib:
+source /opt/ros/humble/setup.bash
+source /home/ubuntu/ros2_ws/install/setup.bash
+ros2 launch choreonoid_ros2_tesrt_sample AizuSpider.launch.xml
