764c46defb28fdbd18ecc028a8cab0159bf81792
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2025-04-22 18:25:45 +0900

    Add files via upload

--- tes_rt_setup.sh
+++ tes_rt_setup.sh
@@ -6,7 +6,7 @@ sudo cp -r /home/ubuntu/tes_regular_tetrahedron/choreonoid_ros2_tesrt_sample /ho
 cd /home/ubuntu/ros2_ws
 colcon build --symlink-install --cmake-args -DBUILD_AGX_DYNAMICS_PLUGIN=ON -DBUILD_AGX_BODYEXTENSION_PLUGIN=ON -DBUILD_WRS2018=ON -DBUILD_SCENE_EFFECTS_PLUGIN=ON -DBUILD_HAIRO_WORLD_PLUGIN=ON -DENABLE_INSTALL_RPATH_USE_LINK_PATH=ON
 
-sudo cp /home/ubuntu/tes_regular_tetrahedron/desktop/TES_RT_AizuSpider.desktop /home/ubuntu/デスクトップ
+sudo cp /home/ubuntu/tes_regular_tetrahedron/desktop/TES_RT_AizuSpider.desktop /home/ubuntu/デスクトップ/TES_RT_AizuSpider.desktop
 gio set /home/ubuntu/デスクトップ/TES_RT_AizuSpider.desktop metadata::trusted true
 sudo chmod +x /home/ubuntu/デスクトップ/TES_RT_AizuSpider.desktop
 
