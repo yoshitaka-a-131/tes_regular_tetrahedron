e4a1e04c83309fa9182063090547440e5f23c057
yoshitaka-a-131 <yoshitaka_a_131@outlook.jp>
2025-04-24 23:11:04 +0900

    Add files via upload

--- tes_rt_setup.sh
+++ tes_rt_setup.sh
@@ -8,9 +8,13 @@ sudo cp -r /home/ubuntu/ros2_ws/src/choreonoid/ext/tes_regular_tetrahedron/chore
 cd /home/ubuntu/ros2_ws
 colcon build --symlink-install --cmake-args -DBUILD_AGX_DYNAMICS_PLUGIN=ON -DBUILD_AGX_BODYEXTENSION_PLUGIN=ON -DBUILD_WRS2018=ON -DBUILD_SCENE_EFFECTS_PLUGIN=ON -DBUILD_HAIRO_WORLD_PLUGIN=ON -DENABLE_INSTALL_RPATH_USE_LINK_PATH=ON
 
-sudo cp /home/ubuntu/ros2_ws/src/choreonoid/ext/tes_regular_tetrahedron/desktop/TES_RT_AizuSpider.desktop /home/ubuntu/デスクトップ/TES_RT_AizuSpider.desktop
-gio set /home/ubuntu/デスクトップ/TES_RT_AizuSpider.desktop metadata::trusted true
-sudo chmod +x /home/ubuntu/デスクトップ/TES_RT_AizuSpider.desktop
+sudo cp /home/ubuntu/ros2_ws/src/choreonoid/ext/tes_regular_tetrahedron/desktop/TES_RT_AizuSpider_1.5m.desktop /home/ubuntu/デスクトップ/TES_RT_AizuSpider_1.5m.desktop
+gio set /home/ubuntu/デスクトップ/TES_RT_AizuSpider_1.5m.desktop metadata::trusted true
+sudo chmod +x /home/ubuntu/デスクトップ/TES_RT_AizuSpider_1.5m.desktop
+
+sudo cp /home/ubuntu/ros2_ws/src/choreonoid/ext/tes_regular_tetrahedron/desktop/TES_RT_AizuSpider_2.0m.desktop /home/ubuntu/デスクトップ/TES_RT_AizuSpider_2.0m.desktop
+gio set /home/ubuntu/デスクトップ/TES_RT_AizuSpider_2.0m.desktop metadata::trusted true
+sudo chmod +x /home/ubuntu/デスクトップ/TES_RT_AizuSpider_2.0m.desktop
 
 sudo cp /home/ubuntu/ros2_ws/src/choreonoid/ext/tes_regular_tetrahedron/desktop/TES_RT_HobbyDrone.desktop /home/ubuntu/デスクトップ/TES_RT_HobbyDrone.desktop
 gio set /home/ubuntu/デスクトップ/TES_RT_HobbyDrone.desktop metadata::trusted true
